71f38b0d50e3395a3814479ecaf0c1e108082839
Matias <matiasrsolis@yahoo.com.ar>
2020-10-06 23:55:50 -0300

    componente producto

--- client/src/components/Producto.jsx
+++ client/src/components/Producto.jsx
@@ -3,13 +3,6 @@ import React from 'react';
 import { producto } from '../../../api/src/models/Product';
 import style from './Producto.module.css';
 
-// Era con fetch?
-function componentDidMount() {
-    fetch({ producto })
-        .then(response => response.json())
-        .then(data => console.log(data));
-}   
-
 export default function Producto ({ name, precio, stock, descripcion }) {
 
     return (
@@ -17,7 +10,7 @@ export default function Producto ({ name, precio, stock, descripcion }) {
           <h5 className={style.titulo}> { name } </h5>
           <button onClick={addPrduct} className={style.btn1}> Añadir al carrito </button>
           <div className={style.infoProducto}>
-              <figure> <img src={foto} alt={ product.name }/> </figure>
+              <figure> <img src={foto} alt={ producto.name }/> </figure>
               <span className={style.precio}> { precio } </span>
               <span className={style.cantidad}> { stock } </span>
               <p className={style.descripcion}> { descripcion } </p>
