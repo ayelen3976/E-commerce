73eff302c8602c9b66c8d97ce93b92cba8371965
Matias <matiasrsolis@yahoo.com.ar>
2020-10-08 19:32:50 -0300

    Catálogo

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import style from './css/ProductCard.module.css';
+import { Route } from 'react-router-dom';
 import {Link} from 'react-router-dom';
 
 export default function ProductCard (props) {
-
+    
     return (
           <div className={style.productCard}>
+              <h1>Product Card</h1>
               <figure> <img src={ props.img } alt={ props.name }/> </figure>
               <div className={style.infoProducto}>
                 <h5 className={style.titulo}> { props.name } </h5>
                 <span className={style.precio}>$ { props.price } </span>
                 <p className={style.descripcion}> { props.description } </p>
-                {/* <Link to={`/producto/${props.id}`}><button onClick={props.addProduct} className={style.btn1}> Ver detalle </button></Link> */}
-                <Link to={`/producto/${props.id}`}><button onClick={props.addProduct} className={style.btn1}> Añadir al carrito </button></Link>
+                <div>
+                <Link to={'/producto/' + props.id}><button onClick={props.addProduct} className={style.btn1}> Ver detalle </button></Link>
+                <Link to={'/producto/' + props.id}><button onClick={props.addProduct} className={style.btn1}> Añadir al carrito </button></Link>
+                </div>
               </div>
           </div>
     );
 };
+
