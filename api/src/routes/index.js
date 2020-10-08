8f6f79369e9b8b2b3909489ded4f44627a0910cd
AyeVillarruel <ayelen.villarruel.1998@gmail.com>
2020-10-08 01:10:46 -0300

    se crearon las rutas de producto y categorias

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require('express');
 const productRouter = require('./product.js');
 
 
+
 const router = Router();
 
 // load each router on a route
 // i.e: router.use('/auth', authRouter);
 // router.use('/auth', authRouter);
 router.use('/products', productRouter);
+
 
 module.exports = router;
--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,143 @@
 const server = require('express').Router();
-const { Product } = require('../db.js');
+const { Product, Category } = require('../db.js');
+
+//READ
 
 server.get('/', (req, res, next) => {
-	Product.findAll()
-		.then(products => {
-			res.send(products);
-		})
-		.catch(next);
+    return Product.findAll()
+        .then(products => {
+            res.json(products);
+        })
+        .catch(err => {
+            res.status(404, err)
+        });
 });
+
+server.get('/category', (req, res, next) => {
+    return Category.findAll()
+         .then(categories => {
+             res.json(categories);
+         })
+         .catch(err => {
+             res.status(404, err)
+        });
+        
+});
+
+
+
+server.get('/category/:nombreCat', (req, res, next) => {
+	const nombreCat = req.params.nombreCat;
+
+	return Category.findAll({
+		where: {
+			name: nombreCat
+		}
+	})
+	.then( categories =>{
+		res.json(categories);
+	})
+	.catch(err =>{
+		res.status(404,err)
+	})
+});
+
+
+
+//CREATE
+
+server.post('/', (req, res, next) => {
+    const {name, description, stock, price}= req.body
+    return Product.create({ name: name, description: description, stock: stock, price: price})
+        .then(producto => {
+            res.status(201).json(producto)
+        })
+        .catch(err => {
+            res.status(404, err)
+        });
+});
+
+
+server.post('/category', (req, res, next) => {
+    const { name, description } = req.body;
+    return Category.create({ name: name, description: description})
+        .then(categoria => {
+            res.status(201).json(categoria)
+        })
+        .catch(err => {
+            res.status(404, err)
+        });
+});
+
+
+//DELETE
+
+server.delete('/:id' , (req,res,next) => {
+    Product.destroy({
+        where: {
+          id: req.params.id
+        }
+      }).then(() => {
+        res.json("Done");
+      })
+});
+
+
+server.delete('/category/:id', (req,res,next) => {
+	Category.destroy({
+        where: {
+          id: req.params.id
+        }
+      }).then(() => {
+        res.json("Done");
+      })
+
+});
+
+//UPDATE
+
+
+server.put('/:id', (req, res, next) => {
+    // Los valores modificados se sacaran del body mas adelante
+    Product.update({
+        price: 500,
+        stock: 10
+    } , {
+        where: {
+            id: req.params.id
+        }
+    }).then(result => {
+        //el update devuelve un array con la cantidad de filas afectadas.
+        res.status(200).json(result);
+    }).catch(err => {
+        res.status(400, err)
+    });
+});
+
+server.put('/category/:id', (req, res, next) => {
+	// Los valores modificados se sacaran del body mas adelante
+	const {name , description} = req.body;
+    Category.update({
+        name: name,
+        description: description
+    } , {
+        where: {
+            id: req.params.id
+        }
+    }).then(result => {
+        //el update devuelve un array con la cantidad de filas afectadas.
+        res.status(200).json(result);
+    }).catch(err => {
+        res.status(400, err)
+    });
+});
+
+
+  
+
+
+
+
+
 
 module.exports = server;
