43c21f8399bf2f4014c6d133a71d61dc682fc0d6
AyeVillarruel <ayelen.villarruel.1998@gmail.com>
2020-10-15 19:49:12 -0300

    registro

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -10,6 +10,7 @@ import FormProducts from './Containers/FormProducts';
 import CategoryForm from './Containers/CategoryForm';
 import CategoryListContainer from './Containers/CategoryListContainer';
 import CategoryInfoContainer from './Containers/CategoryInfoContainer';
+import Register from './Containers/register'
 
 
 
@@ -25,6 +26,7 @@ const Routes = () => {
             <Route exact path='/category' component = {CategoryListContainer}/>
             <Route exact path='/ProductForm' component = {FormProducts}/>
             <Route exact path='/CategoryForm' component = {CategoryForm}/>
+            <Route exact path='/registertest' component = {Register}/>
             
         </Switch>
     )
