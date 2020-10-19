a0d30cd3e9deb2c344991175feefd8adc55d2a5a
ayelen3976 <ayee_01@live.com>
2020-10-19 10:12:16 -0300

    css cart

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -61,7 +61,7 @@ function Nav({ totalItemsInCart }) {
 
 
 const mapStateToProps = (state) => ({
-    totalItemsInCart: Object.keys(state.shopP.cart).length
+    totalItemsInCart: Object.keys(state.shopP.cart).lenght
 })
 
 // export default withStyles({
