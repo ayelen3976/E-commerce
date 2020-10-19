89db956f6b60f6d17310f64d9619dfe40f9603c5
Juan Ignacio <fernandezjubin@gmail.com>
2020-10-18 21:25:49 -0300

    ultimo comit

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -14,17 +14,6 @@ function Nav({ totalItemsInCart }) {
     // const { classes } = props;
 
     return (
-        // <AppBar className={classes.NavColor} position='static'>
-        //     <Toolbar className= {classes.SearchBar} variant='dense'>
-        //         <Typography variant='h6' component='p'>Green Shop</Typography>
-        //         <Link to='/products'>Ver Productos</Link>
-        //         <Link to='/category'>Ver Categorias</Link>
-        //         <Link to='/ProductForm'>Crud Productos</Link>
-        //         <Link to='/CategoryForm'>Crud Categorias</Link>
-        //         <SearchBar/>
-        //     </Toolbar>
-        // </AppBar>
-
         
         <nav class="navbar navbar-expand-lg navbar-light bg-verde" >
             
@@ -64,11 +53,6 @@ const mapStateToProps = (state) => ({
     totalItemsInCart: Object.keys(state.shopP.cart).length
 })
 
-// export default withStyles({
-//     NavColor: {
-//         backgroundColor: '#82ae46'
-//     }
-// })(Nav);
 
 export default connect(mapStateToProps)(Nav);
 
@@ -76,36 +60,3 @@ export default connect(mapStateToProps)(Nav);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// import React from 'react';
-// import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
-// import logo from '../images/logo_size.jpg'
-// import SearchBar from './SearchBar';
-
-// export default function Navvbar() {
-//     return (
-//         <Navbar bg="light" expand="lg">
-//             <Navbar.Brand >  <Link to="/products"> <img src={logo} alt="" width="100px" height='100px' /> </Link></Navbar.Brand>
-//             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//             <Navbar.Collapse id="basic-navbar-nav">
-//                 <Nav className="mr-auto">
-//                     <Nav.Link >Inicio </Nav.Link>
-//                     <Nav.Link >Compra mayorista</Nav.Link>
-//                     <NavDropdown title="Productos" id="basic-nav-dropdown">
-//                         <NavDropdown.Item >Frutos secos</NavDropdown.Item>
-//                         <NavDropdown.Item >Especias</NavDropdown.Item>
-//                         <NavDropdown.Item >Vegetales</NavDropdown.Item>
-//                         <NavDropdown.Item >Lacteos</NavDropdown.Item>
-//                         <NavDropdown.Item >Leches vegetales</NavDropdown.Item>
-//                         <NavDropdown.Item >Sin T.A.C.</NavDropdown.Item>
-//                         <NavDropdown.Item >Legumbres</NavDropdown.Item>
-//                         <NavDropdown.Divider />
-//                         <Nav.Link><Link to="/ProductForm">Formulario Producto</Link></Nav.Link>
-//                         <Nav.Link><Link to="/CategoryForm">Formulario Categorias</Link></Nav.Link>
-//                     </NavDropdown>
-//                 </Nav>
-//                 <SearchBar />
-//             </Navbar.Collapse>
-//         </Navbar>
-//     )
-// }
